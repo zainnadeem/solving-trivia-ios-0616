f4e7a0dd12065e6b1df2d29736b67eb08ac24c44
Joe Burgess <joe@flatironschool.com>
2014-09-30 13:24:26 -0400

    Initial Commit

---
+++ solving-trivia/FISAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FISAppDelegate.h
+//  solving-trivia
+//
+//  Created by iOS Staff on 9/30/14
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
