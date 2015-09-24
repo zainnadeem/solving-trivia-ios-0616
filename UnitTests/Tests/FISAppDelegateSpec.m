7ec5f72fa9940245d368a152aac646e1a2ea7727
markedwardmurray <markedwardmurray@gmail.com>
2015-09-24 17:06:56 -0400

    lab: style updates

--- UnitTests/Tests/FISAppDelegateSpec.m
+++ UnitTests/Tests/FISAppDelegateSpec.m
@@ -1,22 +1,18 @@
-//
 //  FISAppDelegateSpec.m
-//  solving-trivia
-//
-//  Created by Tom OMalley on 5/11/15.
-//  Copyright 2015 The Flatiron School. All rights reserved.
-//
 
 #import "Specta.h"
 #import "FISAppDelegate.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
 
 SpecBegin(FISAppDelegate)
 
 describe(@"FISAppDelegate", ^{
     
     describe(@"solveTrivia method", ^{
-        it(@"returns the only state who's capital contains none of the characters as the state name", ^{
-            FISAppDelegate *delegate = ((FISAppDelegate*)[UIApplication sharedApplication].delegate);
-            expect([delegate solveTrivia]).to.equal(@"South Dakota");
+        it(@"returns the only state whose capital contains none of the characters as the state name", ^{
+            FISAppDelegate *appDelegate = ((FISAppDelegate*)[UIApplication sharedApplication].delegate);
+            expect([appDelegate solveTrivia]).to.equal(@"South Dakota");
         });
     });
     
